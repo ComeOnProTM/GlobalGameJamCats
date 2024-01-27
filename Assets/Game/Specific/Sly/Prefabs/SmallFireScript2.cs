dcf716403bb05a29fdb7b2cb9a2147c4cea336cc
Slyceth <slyceth@gmail.com>
2024-01-27 12:28:11 +0100

    [ADD] Perfect fire spread

--- Assets/Game/Specific/Sly/Prefabs/SmallFireScript2.cs
+++ Assets/Game/Specific/Sly/Prefabs/SmallFireScript2.cs
@@ -5,12 +5,12 @@ using UnityEditor;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
-public class SmallFireScript : MonoBehaviour
+public class SmallFireScript2 : MonoBehaviour
 {
-    [SerializeField] private GameObject FireParticle;
+    [SerializeField]private GameObject FireParticle;
     public float FireTimeA = 5;
     public float FireTimeB = 10;
-    [SerializeField] private float radius = 3f;
+    [SerializeField]private float radius = 3f;
     public LayerMask layermask = 1 << 6;
     private IEnumerator Fire()
     {
@@ -18,20 +18,7 @@ public class SmallFireScript : MonoBehaviour
 
         while (true)
         {
-            float randomValue = Random.value;
-
-            // Check if the random value is greater than 0.5
-            if (randomValue > 0.5f)
-            {
-                // Your code here (this code will be skipped 50% of the time)
-                yield return new WaitForSeconds(Random.Range(FireTimeA, FireTimeB));
-            }
-            else
-            {
-                // This block will be executed when the random value is less than or equal to 0.5
-                Debug.Log("This line of code is skipped.");
-            }
-            
+            yield return new WaitForSeconds(Random.Range(FireTimeA, FireTimeB));
 
             var hitColliders = Physics.OverlapSphere(transform.position, radius, layermask);
             // anything with a collider on the fire layer is affected
@@ -41,16 +28,16 @@ public class SmallFireScript : MonoBehaviour
                 {
                     if (hitCollider.TryGetComponent(out Burnable burnable))
                     {
-
-                        if (burnable.Flamability < Random.Range(0f, 1f))
+                        
+                        if (burnable.Flamability < Random.Range(0f,1f))
                         {
                             SmallFireScript spawnedFire = hitCollider.gameObject.AddComponent<SmallFireScript>();
-                            spawnedFire.FireParticle = FireParticle;
+                           // spawnedFire.FireParticle = FireParticle;
                         }
                     }
                 }
             }
-
+            
             Debug.Log("Loop", context: this);
         }
     }
@@ -64,7 +51,7 @@ public class SmallFireScript : MonoBehaviour
     {
         StartCoroutine(Fire());
 
-        if (FireParticle != null)
+        if(FireParticle != null)
         {
             Instantiate(original: FireParticle, position: transform.position, rotation: transform.rotation);
         }
