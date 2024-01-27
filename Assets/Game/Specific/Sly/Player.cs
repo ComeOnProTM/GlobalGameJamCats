1b6ed454492befdd00116b1c81aba13278b04be0
Slyceth <slyceth@gmail.com>
2024-01-27 12:59:19 +0100

    [ADD] Better camera and dash

--- Assets/Game/Specific/Sly/Player.cs
+++ Assets/Game/Specific/Sly/Player.cs
@@ -1,19 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField]private float speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float dashSpeed;
+    private bool isDashing = false;
 
-    // Update is called once per frame
     void Update()
     {
+        // Regular movement based on keyboard input
         Vector2 inputVector = new Vector2(0, 0);
-        
-        if(Input.GetKey(KeyCode.W)) {
+
+        if (Input.GetKey(KeyCode.W))
+        {
             inputVector.y = 1;
         }
         if (Input.GetKey(KeyCode.A))
@@ -28,9 +29,31 @@ public class Player : MonoBehaviour
         {
             inputVector.x = 1;
         }
+
         inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, inputVector.y, 0);
         transform.position += moveDir * speed * Time.deltaTime;
 
+        // Dash input (you can change this condition based on your input setup)
+        if (Input.GetKeyDown(KeyCode.Space) && !isDashing)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+
+        // Calculate dash direction based on mouse position
+        Vector3 dashDirection = (Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position)).normalized;
+
+        // Move the player in the dash direction with dash speed
+        transform.Translate(dashDirection * dashSpeed * Time.deltaTime);
+
+        // Wait for a short duration for the dash
+        yield return new WaitForSeconds(0.5f);
+
+        isDashing = false;
     }
 }
