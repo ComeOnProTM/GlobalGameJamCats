7c3dfc8e38c33b6329e4b4f83d28dde695f0b6ae
Slyceth <slyceth@gmail.com>
2024-01-28 13:30:56 +0100

    [CHANGE] Textures removed

--- Assets/Game/Specific/Groovy/Scripts/UI/EndGameUI.cs
+++ Assets/Game/Specific/Groovy/Scripts/UI/EndGameUI.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class EndGameUI : MonoBehaviour
+
+    
 {
+    private const string LEVELONE = "CasinoScene";
     [SerializeField] private GameObject fireOutEndGameUI;
     [SerializeField] private GameObject civSavedEndGameUI;
     [SerializeField] private GameObject timerOutEndGameUI;
+
+    [Header("References")]
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
 
     private void Start()
     {
         Hide();
         GameManager.Instance.OnStateChanged += GameManager_OnStateChanged;
+        playButton.onClick.AddListener(() => SceneManager.LoadScene(LEVELONE));
+        quitButton.onClick.AddListener(() => Application.Quit());
+        playButton.Select();
     }
 
     private void GameManager_OnStateChanged(object sender, System.EventArgs e)
