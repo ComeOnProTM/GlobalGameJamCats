df49f13c5f6e281ee49750f14747f969ed5c5334
ComeOnProTM <101826779+ComeOnProTM@users.noreply.github.com>
2024-01-28 08:29:14 +0100

    Groovy I Main Menu

---
+++ Assets/Game/Specific/Groovy/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    private const string LEVELONE = "CasinoScene";
+
+    [Header("References")]
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(() => SceneManager.LoadScene(LEVELONE));
+        playButton.onClick.AddListener(() => Application.Quit());
+    }
+}
